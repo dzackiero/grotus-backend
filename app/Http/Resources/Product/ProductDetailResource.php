7bcad9157166c5de7557e230966108de81202e77
dzackiero <dzakynashshar@gmail.com>
2024-06-20 13:12:38 +0700

    fix missing id product detail

--- app/Http/Resources/Product/ProductDetailResource.php
+++ app/Http/Resources/Product/ProductDetailResource.php
@@ -17,6 +17,7 @@ class ProductDetailResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            "id" => $this->id,
             "name" => $this->name,
             "price" => $this->price,
             "stock" => $this->stock,
